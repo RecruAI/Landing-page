4657e06ab247bbae290aa92693117d3bfded4997
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-05-25 23:31:14 +0200

    Authentication

---
+++ components/landing_page/authUi.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supabase/auth-ui-react";
+import { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+export default function AuthUi(props: { closeAuthUi: Function }) {
+	const supabase = createClientComponentClient();
+
+	const [theme, setTheme] = useState(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+	useEffect(() => {
+		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+			setTheme(event.matches);
+		});
+	}, []);
+
+	return (
+		<>
+			<section className="absolute z-20 bg-[--background-rgb] border-2 border-colorGray p-8 rounded-2xl top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+				<Auth
+					supabaseClient={supabase}
+					appearance={{
+						theme: ThemeSupa,
+						variables: {
+							default: {
+								colors: {
+									brand: "rgb(51, 97, 227)",
+									brandAccent: "rgb(41, 87, 217)",
+								},
+							},
+						},
+					}}
+					providers={["google", "facebook", "twitter"]}
+					theme={theme ? "dark" : "default"}
+				/>
+			</section>
+			<div className="w-screen h-screen z-10 bg-black/30" onClick={() => props.closeAuthUi()}></div>
+		</>
+	);
+}
--- components/landing_page/header.tsx
+++ components/landing_page/header.tsx
@@ -1,15 +1,68 @@
+"use client";
+
 import { NextFont } from "next/dist/compiled/@next/font";
 import Image from "next/image";
+import AuthUi from "./authUi";
+import { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function Header(props: { font: NextFont }) {
-	return (
-		<header className="w-screen QHD:w-3/4 WQHD:w-2/3 mx-auto flex justify-between py-5 px-10 content-center items-center fixed">
-			<Image src="/landing_page/Icon-Light.svg" width="150" height="0" alt="" />
+	const [authUiShowed, toggleAuthUi] = useState(false);
+	const [signedIn, setSignIn] = useState(false);
 
-			<div className={"flex gap-x-4 " + props.font.className}>
-				<button className={"text-colorGray h-fit text-lg font-normal tracking-wider"}>Sign in</button>
-				<button className={"text-colorBlue h-fit text-lg font-medium tracking-wider"}>Sign up</button>
-			</div>
-		</header>
+	const supabase = createClientComponentClient();
+
+	function closeAuthUi() {
+		toggleAuthUi(false);
+	}
+
+	async function signOut() {
+		const { error } = await supabase.auth.signOut();
+		setSignIn(false);
+		toggleAuthUi(false);
+	}
+
+	async function checkIfLogged() {
+		const { data, error } = await supabase.auth.getSession();
+		console.log(data);
+		if (data) setSignIn(true);
+	}
+
+	useEffect(() => {
+		checkIfLogged();
+
+		supabase.auth.onAuthStateChange(async (event, session) => {
+			console.log(event);
+
+			if (event == "SIGNED_IN") {
+				setSignIn(true);
+				toggleAuthUi(false);
+			}
+		});
+	}, []);
+
+	return (
+		<>
+			<header className="w-screen QHD:w-3/4 WQHD:w-2/3 mx-auto flex justify-between py-5 px-10 content-center items-center left-1/2 -translate-x-1/2 fixed">
+				<Image src="/landing_page/Icon-Light.svg" width="150" height="0" alt="" />
+
+				<div className={"flex gap-x-5 items-center " + props.font.className}>
+					{!signedIn ? (
+						<button className={"text-colorBlue h-fit text-xl tracking-wider"} onClick={() => toggleAuthUi(true)}>
+							Sign In
+						</button>
+					) : (
+						<>
+							<button className={"text-colorGray h-fit text-lg tracking-wider"} onClick={() => signOut()}>
+								Log Out
+							</button>
+							<button className={"text-white bg-colorBlue rounded-md px-8 py-3 text-xl tracking-widest"}>Start</button>
+						</>
+					)}
+				</div>
+			</header>
+
+			{authUiShowed ? <AuthUi closeAuthUi={closeAuthUi} /> : <> </>}
+		</>
 	);
 }
