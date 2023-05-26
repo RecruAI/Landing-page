3f36e2760f3b37fc840a985c835403f0ca115f6e
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-05-26 23:31:56 +0200

    Section two

--- components/landing_page/sectionTwo/sectionTwo.tsx
+++ components/landing_page/sectionTwo/sectionTwo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function SectionTwo(props: { font: NextFont }) {
 	return (
 		<section className="flex flex-col lg:flex-row justify-evenly responsiveWidth items-center">
-			<div className="flex flex-col basis-5/12 text-start gap-y-4 sm:gap-y-8">
+			<div className="flex flex-col lg:basis-5/12 text-start gap-y-4 sm:gap-y-8">
 				<span className="text-lg sm:text-xl bg-[#F9F1EC] text-[#C3995D] w-fit font-medium px-3 py-1 rounded-md">Headings</span>
 				<h3 className={"text-[--text-rgb] text-3xl sm:text-6xl lg:text-5xl xl:text-7xl " + props.font.className}>Divide and conquer</h3>
 				<p className="text-colorGray text-sm  sm:text-lg text-justify">
@@ -13,15 +13,15 @@ export default function SectionTwo(props: { font: NextFont }) {
 				</p>
 			</div>
 
-			<div className="flex overflow-hidden relative basis-7/12">
-				{/* <picture className="translate-y-8 translate-x-10 lg:translate-x-0">
-					<source srcSet="/landing_page/images/SectionThreeDark-1.png" media="(prefers-color-scheme: dark)" />
-					<Image src="/landing_page/images/SectionThreeLight-1.png" width="300" height="300" alt="Moble app image" />
+			<div className="flex  flex-col items-end relative lg:basis-7/12 gap-y-5 lg:scale-75 xl:scale-100">
+				<picture className="-translate-x-14 translate-y-10 xl:-translate-x-24">
+					<source srcSet="/landing_page/images/SectionTwoDark-1.png" media="(prefers-color-scheme: dark)" />
+					<Image src="/landing_page/images/SectionTwoLight-1.png" width="500" height="500" alt="Moble app image" className="rounded-xl" />
 				</picture>
-				<picture className="translate-y-40 -translate-x-10 lg:-translate-x-20">
-					<source srcSet="/landing_page/images/SectionThreeDark-2.png" media="(prefers-color-scheme: dark)" />
-					<Image src="/landing_page/images/SectionThreeLight-2.png" width="300" height="300" alt="Moble app image" />
-				</picture> */}
+				<picture className="z-10 -translate-y-10 translate-x-10 xl:translate-x-0">
+					<source srcSet="/landing_page/images/SectionTwoDark-2.png" media="(prefers-color-scheme: dark)" />
+					<Image src="/landing_page/images/SectionTwoLight-2.png" width="350" height="500" alt="Moble app image" className="rounded-xl" />
+				</picture>
 			</div>
 		</section>
 	);
