40c950d2dd7cbf3398cb5025cd17b256d328b483
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-05-27 18:23:06 +0200

    Section five Design of section five without images (for now)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/landing_page/footer";
 import Header from "@/components/landing_page/header";
+import SectionFive from "@/components/landing_page/sectionFive/sectionFive";
 import SectionFour from "@/components/landing_page/sectionFour/sectionFour";
 import SectionOne from "@/components/landing_page/sectionOne/sectionOne";
 import SectionThree from "@/components/landing_page/sectionThree/sectionThree";
@@ -19,8 +20,9 @@ export default function Home() {
 				<SectionThree font={EuropaGrotesk} />
 			</div>
 
-			<div className="px-10 md:px-20 lg:px-24 xl:px-32 w-screen relative py-40 bg-gradient-to-br gap-y-32 text-center justify-center items-center flex flex-col from-[--background-rgb] to-[--background-rgb-darker]">
+			<div className="px-10 md:px-20 lg:px-24 xl:px-32 w-screen relative pt-40 bg-gradient-to-br text-center justify-center items-center flex flex-col from-[--background-rgb] to-[--background-rgb-darker]">
 				<SectionFour font={EuropaGrotesk} />
+				<SectionFive font={EuropaGrotesk} />
 			</div>
 			<Footer font={EuropaGrotesk} />
 		</main>
---
+++ components/landing_page/sectionFive/sectionFive.tsx
@@ -0,0 +1,16 @@
+import { NextFont } from "next/dist/compiled/@next/font";
+import Image from "next/image";
+
+export default function SectionFive(props: { font: NextFont }) {
+	return (
+		<section className="flex flex-col responsiveWidth gap-y-4 sm:gap-y-8 items-center mt-44">
+			<span className="text-lg sm:text-xl bg-[#EDF4EF] text-[#76B689] w-fit font-medium px-3 py-1 rounded-md">Testemonials</span>
+			<h3 className={"text-[--text-rgb] text-3xl sm:text-6xl lg:text-5xl xl:text-7xl " + props.font.className}>Users love do it</h3>
+			<p className="text-colorGray text-sm sm:text-lg md:text-xl max-w-3xl tracking-wide ">
+				"Do It helps keep me organized - I truly could not live without it! Allows me to prioritize my tasks in a super simple way. I definitely recommend Do It to
+				everyone - I really think it's the best to-do list app available!"
+			</p>
+			<span className={"text-[--text-rgb] text-base sm:text-xl md:text-2xl tracking-wider " + props.font.className}>- Andrew Cano</span>
+		</section>
+	);
+}
