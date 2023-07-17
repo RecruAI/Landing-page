e8f0c0438542fab65be198cde7890b0a64db9d65
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-07-17 02:01:33 +0200

    Real time lists updates fixed

--- components/app/listsContainer.tsx
+++ components/app/listsContainer.tsx
@@ -4,7 +4,6 @@ import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { PostgrestSingleResponse } from "@supabase/supabase-js";
-import { RealtimePostgresChangesPayload } from "@supabase/realtime-js/dist/module/RealtimeChannel";
 import { useEffect, useState } from "react";
 
 type dataType = {
@@ -17,14 +16,26 @@ type dataType = {
 
 export default function ListsContainer() {
 	const [lists, setLists] = useState<dataType>([]);
+	const [loading, setLoading] = useState<boolean>(true);
 
 	const supabase = createClientComponentClient({});
 
+	function handleListsChange(payload: any) {
+		if (payload.eventType === "DELETE") {
+			setLists((prevLists) => prevLists.filter((item) => item.id !== payload.old.id));
+		} else if (payload.eventType === "INSERT") {
+			setLists((prevLists) => [payload.new, ...prevLists]);
+		} else {
+			setLists((prevLists) => prevLists.map((item) => (item.id === payload.old.id ? payload.new : item)));
+		}
+	}
+
 	useEffect(() => {
 		async function fetchData() {
-			let { data: lists, error }: PostgrestSingleResponse<dataType> = await supabase.from("lists").select("*");
+			let { data: lists }: PostgrestSingleResponse<dataType> = await supabase.from("lists").select("*");
 
 			setLists(lists != undefined && lists != null ? lists : []);
+			setLoading(false);
 		}
 
 		fetchData();
@@ -33,29 +44,18 @@ export default function ListsContainer() {
 			.channel("any")
 			.on("postgres_changes", { event: "*", schema: "public", table: "lists" }, (payload) => handleListsChange(payload))
 			.subscribe();
-
-		function handleListsChange(payload: any) {
-			if (payload.eventType == "DELETE") {
-				const newArray = lists.filter(function (item: any) {
-					return item.id != payload.old.id;
-				});
-				setLists(newArray);
-			} else if (payload.eventType == "INSERT") {
-				let newArray = lists;
-				newArray.unshift(payload.new);
-				setLists(newArray);
-			} else {
-				let objIndex: number = lists.findIndex((obj) => obj.id == payload.old.id);
-				let newArray = lists;
-				newArray[objIndex] = payload.new;
-				setLists(newArray);
-			}
-		}
 	}, []);
 
 	return (
 		<>
-			{lists.length != 0 ? (
+			{loading ? (
+				// Showing loading before fetched data from db
+				<button className="listButton">
+					<FontAwesomeIcon fixedWidth icon={faSpinner} className="h-8 w-8 p-1.5 text-colorGray" />
+
+					<p className="text-[--text-rgb]">Loading...</p>
+				</button>
+			) : (
 				lists.map((list) => {
 					return (
 						<button className="listButton" key={list.id}>
@@ -68,13 +68,6 @@ export default function ListsContainer() {
 						</button>
 					);
 				})
-			) : (
-				// Showing loading before fetched data from db
-				<button className="listButton">
-					<FontAwesomeIcon fixedWidth icon={faSpinner} className="h-8 w-8 p-1.5 text-colorGray" />
-
-					<p className="text-[--text-rgb]">Loading...</p>
-				</button>
 			)}
 		</>
 	);
