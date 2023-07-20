0e4829e13eb8f408c7c4b42662d6bb87c941f071
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-07-20 02:33:35 +0200

    Global.css cleaned

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,10 +22,12 @@ module.exports = {
 			},
 			margin: {
 				"20vw": "20vw",
+				"7px": "7px",
 			},
 			padding: { 0.75: "0.1875rem" },
 			width: { xlFull: "calc(100vw - 18rem)" },
 			spacing: { 0.75: "3px" },
+
 			animation: {
 				"shrink-bounce": "shrink-bounce 200ms cubic-bezier(.4,.0,.23,1)",
 				"checkbox-check": "checkbox-check 125ms 250ms cubic-bezier(.4,.0,.23,1) forwards",
