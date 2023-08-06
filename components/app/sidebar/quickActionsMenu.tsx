5467d9ed7827463cb6653754aad747585c5fd1f7
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-08-06 17:37:17 +0200

    Quick actions menu changes, routes created

--- components/app/sidebar/quickActionsMenu.tsx
+++ components/app/sidebar/quickActionsMenu.tsx
@@ -2,8 +2,14 @@
 
 import { faBoxArchive, faCalendar, faCalendarCheck, faHome, faInbox, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function QuickActionsMenu() {
+	const [today, setToday] = useState<number | undefined>(undefined);
+	const [nextWeek, setNextWeek] = useState<number | undefined>(undefined);
+	const [forgotten, setForgotten] = useState<number | undefined>(0);
+
 	return (
 		<>
 			<div className="flex flex-col gap-y-1">
@@ -11,30 +17,30 @@ export default function QuickActionsMenu() {
 					<FontAwesomeIcon fixedWidth icon={faMagnifyingGlass} className="h-8 w-8 p-1.5 text-colorGray" />
 					<p className="text-[--text-rgb]">Quick find</p>
 				</button>
-				<button className="sidebarButton">
+				<Link href={"/app"} className="sidebarButton">
 					<FontAwesomeIcon fixedWidth icon={faHome} className="h-8 w-8 p-1.5 text-[#4B81EB]" />
 					<p className="text-[--text-rgb]">Home</p>
-				</button>
+				</Link>
 			</div>
 
 			<div className="flex flex-col gap-y-1">
-				<button className="sidebarButton">
+				<Link href={"/app/quick/today"} className="sidebarButton">
 					<FontAwesomeIcon fixedWidth icon={faCalendarCheck} className="h-8 w-8 p-1.5 text-[#46BF77]" />
 					<p className="text-[--text-rgb]">Today</p>
-					<p className="ms-auto text-colorGray">21</p>
-				</button>
+					<p className="ms-auto text-colorGray">{today ?? ""}</p>
+				</Link>
 
-				<button className="sidebarButton">
+				<Link href={"/app/quick/nextweek"} className="sidebarButton">
 					<FontAwesomeIcon fixedWidth icon={faCalendar} className="h-8 w-8 p-1.5 text-[#9D59DF]" />
-					<p className="text-[--text-rgb]">Upcoming</p>
-					<p className="ms-auto text-colorGray">9</p>
-				</button>
+					<p className="text-[--text-rgb]">Next week</p>
+					<p className="ms-auto text-colorGray">{nextWeek ?? ""}</p>
+				</Link>
 
-				<button className="sidebarButton">
-					<FontAwesomeIcon fixedWidth icon={faBoxArchive} className="h-8 w-8 p-1.5 text-[#DF8637]" />
-					<p className="text-[--text-rgb]">Someday</p>
-					<p className="ms-auto text-colorGray">2</p>
-				</button>
+				<Link href={"/app/quick/forgotten"} className="sidebarButton">
+					<FontAwesomeIcon fixedWidth icon={faBoxArchive} className="h-8 w-8 p-1.5 text-[#df3737]" />
+					<p className="text-[--text-rgb]">Forgotten</p>
+					<p className="ms-auto text-colorGray">{forgotten ?? ""}</p>
+				</Link>
 			</div>
 		</>
 	);
